c6d46625facc748a73c0d1338a37c550ba045e3c
ethan-ou <45083970+ethan-ou@users.noreply.github.com>
2020-06-25 13:04:23 +1000

    Add sharing capability

--- src/screens/QuestionScreen.tsx
+++ src/screens/QuestionScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableHighlight, Share } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import AsyncStorage from "@react-native-community/async-storage";
 
@@ -11,11 +11,6 @@ import { addQuestions, loadQuestions, switchTheme } from "../store/actions";
 import { StackParamList, Routes } from "../types";
 import Button from "../components/Button";
 import theme from "../constants/themes";
-
-// back arrow instead of refresh
-// home icon instead of speech bubble
-
-//Change background to a moving gradient
 
 interface Props {
   navigation: StackNavigationProp<StackParamList, Routes>;
@@ -59,6 +54,16 @@ export default function Question(props: Props) {
 
   const navigateMenu = () => props.navigation.navigate("Menu");
 
+  const shareQuestion = async () => {
+    try {
+      const result = await Share.share({
+        message: state.questions[count].question,
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
     <View
       style={[
@@ -68,34 +73,37 @@ export default function Question(props: Props) {
         },
       ]}
     >
-      <MenuBar
-        themeHandler={handleTheme}
-        menuHandler={navigateMenu}
-        undoHandler={decrementCount}
-      />
-      <View style={styles.top}>
-        <Text
-          style={[
-            themeStyle("primary"),
-            styles.fontSize7,
-            styles.fontBold,
-            styles.marginT4,
-          ]}
-        >
-          {capitalise(state.questions[count].category)}
-        </Text>
-
-        <Text
-          style={[
-            styles.marginB2,
-            themeStyle("primary", "secondary"),
-            styles.fontSize3,
-            styles.marginT1p5,
-          ]}
-        >
-          {state.questions[count].question}
-        </Text>
+      <View style={[styles.marginB4]}>
+        <MenuBar
+          themeHandler={handleTheme}
+          menuHandler={navigateMenu}
+          undoHandler={decrementCount}
+        />
       </View>
+      <TouchableHighlight
+        onLongPress={shareQuestion}
+        underlayColor={"transparent"}
+        activeOpacity={0.4}
+      >
+        <View style={styles.top}>
+          <Text
+            style={[themeStyle("primary"), styles.fontSize7, styles.fontBold]}
+          >
+            {capitalise(state.questions[count].category)}
+          </Text>
+
+          <Text
+            style={[
+              styles.marginB2,
+              themeStyle("primary", "secondary"),
+              styles.fontSize3,
+              styles.marginT1p5,
+            ]}
+          >
+            {state.questions[count].question}
+          </Text>
+        </View>
+      </TouchableHighlight>
       <View style={[styles.bottom, styles.paddingV2]}>
         <Button onPress={incrementCount} text="Next" />
       </View>
