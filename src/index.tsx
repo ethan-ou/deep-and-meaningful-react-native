f5f02c2d7c45129f52a5804243019cdef9330895
ethan-ou <45083970+ethan-ou@users.noreply.github.com>
2020-07-30 17:48:55 +1000

    Add splash screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import "react-native-gesture-handler";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Appearance } from "react-native-appearance";
+import * as SplashScreen from "expo-splash-screen";
 
 import { useAppContext } from "./store";
 import Home from "./screens/HomeScreen";
@@ -14,54 +15,76 @@ import AsyncStorage from "@react-native-community/async-storage";
 import { SafeAreaView, StatusBar } from "react-native";
 import { color } from "./constants/themes";
 
+const getThemeFromStorage = async () => {
+  try {
+    const value = await AsyncStorage.getItem("theme");
+    if (value !== null) {
+      return value;
+    }
+
+    const theme = Appearance.getColorScheme();
+    if (theme !== null && (theme === "light" || theme === "dark")) {
+      return theme;
+    }
+  } catch (e) {}
+};
+
+const getCategoryFromStorage = async () => {
+  try {
+    const value = await AsyncStorage.getItem("category");
+    if (value !== null) {
+      return value;
+    }
+  } catch (e) {}
+};
+
+const delaySplashScreen = async () => {
+  try {
+    await SplashScreen.preventAutoHideAsync();
+  } catch (e) {
+    console.warn(e);
+  }
+};
+
+const showSplashScreen = async () => {
+  setTimeout(async () => {
+    await SplashScreen.hideAsync();
+  }, 100);
+};
+
 export default function Index() {
   const [state, dispatch] = useAppContext();
+  const [matchTheme, setMatchTheme] = useState("");
   const colorStyle = color(state.theme);
 
   const _isMounted = useRef(false);
 
-  const getTheme = async () => {
-    try {
-      const value = await AsyncStorage.getItem("theme");
-      if (value !== null) {
-        return value;
-      }
-
-      const theme = Appearance.getColorScheme();
-      if (theme !== null && (theme === "light" || theme === "dark")) {
-        return theme;
-      }
-    } catch (e) {}
-  };
-
-  const getCategory = async () => {
-    try {
-      const value = await AsyncStorage.getItem("category");
-      if (value !== null) {
-        return value;
-      }
-    } catch (e) {}
-  };
-
   useEffect(() => {
     _isMounted.current = true;
 
-    getTheme().then((value) => {
-      if (value && _isMounted.current) {
-        dispatch(switchTheme(JSON.parse(value)));
-      }
-    });
+    delaySplashScreen().then(() => {
+      getThemeFromStorage().then((value) => {
+        if (value && _isMounted.current) {
+          dispatch(switchTheme(JSON.parse(value)));
+          setMatchTheme(JSON.parse(value));
+        }
+      });
 
-    getCategory().then((value) => {
-      if (value && _isMounted.current) {
-        dispatch(selectCategory(JSON.parse(value)));
-      }
+      getCategoryFromStorage().then((value) => {
+        if (value && _isMounted.current) {
+          dispatch(selectCategory(JSON.parse(value)));
+        }
+      });
     });
 
     return () => {
       _isMounted.current = false;
     };
   }, []);
+
+  if (matchTheme === state.theme) {
+    showSplashScreen();
+  }
 
   const Stack = createStackNavigator<StackParamList>();
   return (
